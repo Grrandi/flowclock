7a7e9bd468f7e6e217f3618417f59be58c10e135
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2017-06-02 11:32:42 +0300

    add time calculations and basic user info

--- FlowClock.elm
+++ FlowClock.elm
@@ -1,6 +1,7 @@
-import Html exposing (Html, p, text, button, div, h1)
+import Html exposing (Html, p, text, button, div, h1, span, input)
 import Time exposing (Time, second, millisecond)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (placeholder)
 
 
 main : Program Never Model Msg
@@ -19,12 +20,14 @@ main =
 type State = Running | Stopped | Disturbed
 type Concentration = JustStarted | GettingThere | WorkingOnIt | OnFire | Ommmmm
 
-type alias Model = {tick : Time, runningSeconds : Int, state : State, concentration : Concentration}
+type alias User = {name : String, hourlySalary : Int}
+
+type alias Model = {tick : Time, runningSeconds : Int, state : State, concentration : Concentration, user : User}
 
 
 init : (Model, Cmd Msg)
 init =
-  ({tick = 0, runningSeconds = 0, state = Stopped, concentration = JustStarted}, Cmd.none)
+  ({tick = 0, runningSeconds = 0, state = Stopped, concentration = JustStarted, user = {name = "", hourlySalary = 0}}, Cmd.none)
 
 
 
@@ -32,7 +35,7 @@ init =
 
 
 type Msg
-  = Tick Time | Start | Stop
+  = Tick Time | Start | Stop | Name String | Salary String
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -40,16 +43,36 @@ update msg model =
   case msg of
     Tick newTime ->
       let
-          newModel = {model | tick = newTime}
+        newModel = {model | tick = newTime}
       in
-          someFunction newModel
+        someFunction newModel
     Start ->
       ({model | runningSeconds = 0, state = Running}, Cmd.none)
     Stop ->
       let
-          newModel = {model | state = Disturbed}
+        newModel = {model | state = Disturbed}
       in
-          someFunction newModel
+        someFunction newModel
+    Name n ->
+      let
+        setUserName = \user n -> {user | name = n}
+        oldUser = model.user
+        updatedUser = setUserName oldUser n
+      in
+        ({model | user = updatedUser}, Cmd.none)
+    Salary s ->
+      case String.toInt s of
+        Err err->
+          (model, Cmd.none)
+        Ok s ->
+          let
+            setUserSalary = \user s -> {user | hourlySalary = s}
+            oldUser = model.user
+            updatedUser = setUserSalary oldUser s
+          in
+            ({model | user = updatedUser}, Cmd.none)
+
+
 
 updateConcentration : Model -> Model
 updateConcentration model =
@@ -99,6 +122,34 @@ showProductivity model =
     case model.concentration of
       _ -> p [] [ text "fufufu"]
 
+
+showTime : Model -> Html Msg
+showTime model =
+  if model.state == Stopped then
+      p [] []
+  else
+    let
+      minutes = model.runningSeconds // 60
+      seconds = rem model.runningSeconds 60
+    in
+      p []
+      [ span [] [text "distufuhaf: "]
+      , span [] [text (String.padLeft 2 '0' <| toString minutes)]
+      , span [] [text ":"]
+      , span [] [text (String.padLeft 2 '0' <| toString seconds)]]
+
+
+showUserInfo : Model -> Html Msg
+showUserInfo model =
+    if model.state == Stopped then
+      div []
+        [ input [ placeholder "Name", onInput Name] []
+        , input [ placeholder "Salary", onInput Salary] []
+        ]
+    else
+      div [] []
+
+
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
@@ -116,7 +167,9 @@ view model =
     in
       div []
         [ h1 [] [ text "Flowclock - Productivity Counter" ]
+        , ( showUserInfo model)
         , ( showProductivity model)
+        , ( showTime model)
         , p [] [text (toString model)]
         , p [] [text (toString model.runningSeconds)]
         , ( showButton model)
