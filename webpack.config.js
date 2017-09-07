f56513d0a420a61564d760bbc99046c4211980e4
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2017-09-07 11:15:43 +0300

    port to use elm webpack boilerplate template

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,129 @@
-var path = require("path");
+var path = require('path');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app: [
-      './src/index.js'
+
+const prod = 'production';
+const dev = 'development';
+
+// determine build env
+const TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? prod : dev;
+const isDev = TARGET_ENV == dev;
+const isProd = TARGET_ENV == prod;
+
+// entry and output path/filename variables
+const entryPath = path.join(__dirname, 'src/static/index.js');
+const outputPath = path.join(__dirname, 'dist');
+const outputFilename = isProd ? '[name]-[hash].js' : '[name].js'
+
+console.log('WEBPACK GO! Building for ' + TARGET_ENV);
+
+// common webpack config (valid for dev and prod)
+var commonConfig = {
+    output: {
+        path: outputPath,
+        filename: `static/js/${outputFilename}`,
+    },
+    resolve: {
+        extensions: ['.js', '.elm'],
+        modules: ['node_modules']
+    },
+    module: {
+        noParse: /\.elm$/,
+        rules: [{
+            test: /\.(eot|ttf|woff|woff2|svg)$/,
+            use: 'file-loader?publicPath=../../&name=static/css/[hash].[ext]'
+        }]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [autoprefixer()]
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/static/index.html',
+            inject: 'body',
+            filename: 'index.html'
+        })
     ]
-  },
+}
 
-  output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js',
-  },
+// additional webpack settings for local env (when invoked by 'npm start')
+if (isDev === true) {
+    module.exports = merge(commonConfig, {
+        entry: [
+            'webpack-dev-server/client?http://localhost:8080',
+            entryPath
+        ],
+        devServer: {
+            // serve index.html in place of 404 responses
+            historyApiFallback: true,
+            contentBase: './src',
+            hot: true
+        },
+        module: {
+            rules: [{
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: [{
+                    loader: 'elm-webpack-loader',
+                    options: {
+                        verbose: true,
+                        warn: true,
+                        debug: true
+                    }
+                }]
+            },{
+                test: /\.sc?ss$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            }]
+        }
+    });
+}
 
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss|less)$/,
-        use: [
-          'style-loader',
-          'css-loader',
+// additional webpack settings for prod env (when invoked via 'npm run build')
+if (isProd === true) {
+    module.exports = merge(commonConfig, {
+        entry: entryPath,
+        module: {
+            rules: [{
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: 'elm-webpack-loader'
+            }, {
+                test: /\.sc?ss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader', 'less-loader']
+                })
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin({
+                filename: 'static/css/[name]-[hash].css',
+                allChunks: true,
+            }),
+            new CopyWebpackPlugin([{
+                from: 'src/static/img/',
+                to: 'static/img/'
+            }, {
+                from: 'src/favicon.ico'
+            }]),
+
+            // extract CSS into a separate file
+            // minify & mangle JS/CSS
+            new webpack.optimize.UglifyJsPlugin({
+                minimize: true,
+                compressor: {
+                    warnings: false
+                }
+                // mangle:  true
+            })
         ]
-      },
-      {
-        test:    /\.html$/,
-        exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
-      },
-      {
-        test:    /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true',
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-      },
-    ],
-
-    noParse: /\.elm$/,
-  },
-
-  devServer: {
-    inline: true,
-    stats: { colors: true },
-  },
-
-
-};
+    });
+}
