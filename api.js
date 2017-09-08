ecad1e4d0773e5d2c8d1d146a834025d7ac6dd6c
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2017-09-08 10:10:24 +0300

    comment api plan

--- api.js
+++ api.js
@@ -10,20 +10,25 @@ app.get('/', function (req, res) {
 });
 
 app.push('/register', function (req, res) {
+  // tee jotain requestilla
+  // db.push("/riksa", {"salasana": "kissa123", "tuntipalkka": 12})
   res.send('Hello World!')
 });
 
 app.push('/login', function (req, res) {
+  // kato käyttätunnus
+  // hae käyttätunnukselle salasana
+  // jos täsmää palauta kaikki kirjaukset
+  // jos ei niin error tms
   res.send('Hello World!')
 });
 
 app.push('/log', function (req, res) {
+  // kirjaa käyttäjtunnukselle uusi flow
+  // palauta tallennuksen jälkeen kaikki käyttäjän flowt ja elm rakentaa listan uusiksi
   res.send('Hello World!')
 });
 
-app.get('/user-data', function (req, res) {
-  res.send('Hello World!')
-});
 
 app.listen(8081, function () {
   console.log('Example app listening on port 3000!')
