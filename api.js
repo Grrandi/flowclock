8e426d25a4c237e3991f8ea0152399bf1e6b7bcb
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2017-09-08 11:15:58 +0300

    implement login and register for api

--- api.js
+++ api.js
@@ -1,17 +1,31 @@
 const JsonDB = require('node-json-db');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var db = new JsonDB("FlowClockDB", true, false);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.post('/', function (req, res) {
+  res.send(req.body.foo)
 });
 
 app.post('/register', function (req, res) {
   // tee jotain requestilla
   // db.push("/riksa", {"salasana": "kissa123", "tuntipalkka": 12})
-  res.send('Hello World!')
+  username = req.body.username;
+  password = req.body.password;
+  salary = req.body.salary;
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    var data = db.getData("/" + username);
+    res.status(403).send(JSON.stringify({"error": "User already exists"}))
+  } catch(error) {
+    db.push("/" + username, {"password": password, "salary": salary, "flows": []});
+    res.status(200).send(JSON.stringify({"flows": []}))
+  }
 });
 
 app.post('/login', function (req, res) {
@@ -19,10 +33,25 @@ app.post('/login', function (req, res) {
   // hae käyttätunnukselle salasana
   // jos täsmää palauta kaikki kirjaukset
   // jos ei niin error tms
-  res.send('Hello World!')
+  username = req.body.username;
+  password = req.body.password;
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    var data = db.getData("/" + username);
+    if(data.password === password) {
+      res.status(200).send(JSON.stringify(data.flows));
+    } else {
+      res.status(403).send(JSON.stringify({"error": "Password incorrect"}))
+    }
+  } catch(error) {
+    res.status(404).send(JSON.stringify({"error": "User does not exist"}))
+  }
 });
 
 app.post('/log', function (req, res) {
+  seconds = req.body.seconds;
+  username = req.body.username;
+  
   // kirjaa käyttäjtunnukselle uusi flow
   // palauta tallennuksen jälkeen kaikki käyttäjän flowt ja elm rakentaa listan uusiksi
   res.send('Hello World!')
