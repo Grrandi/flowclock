8cdda0013cb5cb2aff93eda042801a2e223183df
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2017-09-08 10:18:17 +0300

    fix

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
-const jsonServer = require('json-server')
-const jsonDB = require('node-json-db');
-const express = require('express')
-const app = express()
+const JsonDB = require('node-json-db');
+const express = require('express');
+const app = express();
 
 var db = new JsonDB("FlowClockDB", true, false);
 
@@ -9,13 +8,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.push('/register', function (req, res) {
+app.post('/register', function (req, res) {
   // tee jotain requestilla
   // db.push("/riksa", {"salasana": "kissa123", "tuntipalkka": 12})
   res.send('Hello World!')
 });
 
-app.push('/login', function (req, res) {
+app.post('/login', function (req, res) {
   // kato käyttätunnus
   // hae käyttätunnukselle salasana
   // jos täsmää palauta kaikki kirjaukset
@@ -23,7 +22,7 @@ app.push('/login', function (req, res) {
   res.send('Hello World!')
 });
 
-app.push('/log', function (req, res) {
+app.post('/log', function (req, res) {
   // kirjaa käyttäjtunnukselle uusi flow
   // palauta tallennuksen jälkeen kaikki käyttäjän flowt ja elm rakentaa listan uusiksi
   res.send('Hello World!')
@@ -31,5 +30,5 @@ app.push('/log', function (req, res) {
 
 
 app.listen(8081, function () {
-  console.log('Example app listening on port 3000!')
-}
+  console.log('Example app listening on port 8081!')
+});
